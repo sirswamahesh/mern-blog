3ca6957c4bc05d646c0a56a8805b6bb510229328
sirswamahesh <sirswamahesh@gmail.com>
2024-06-28 18:35:25 +0530

    add some functionally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import FooterCom from './components/Footer'
 const App = () => {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<SignUp/>} />
         </Routes>
+        <FooterCom />
     </BrowserRouter>
   )
 }
