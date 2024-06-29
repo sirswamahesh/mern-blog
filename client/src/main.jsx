af5399eae2b5864797e1935a0be2613a90ea6dbb
sirswamahesh <sirswamahesh@gmail.com>
2024-06-29 18:38:42 +0530

    create global state of theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from 'react-redux'
 import { store } from './redux/store.js'
+import ThemeProvider from './components/ThemeProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <ThemeProvider>
+
     <App />
+    </ThemeProvider>
   </Provider>,
 )
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -39,17 +39,17 @@ export default function SignIn() {
     }
   };
   return (
-    <div className='min-h-screen mt-20'>
+    <div className='min-h-screen pt-20 dark:bg-[rgb(16,23,42)]'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
         <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-4xl'>
+          <Link to='/' className='font-bold dark:text-white dark:bg-[rgb(16,23,42)] text-4xl '>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
              Hunting
             </span>
             Blog
           </Link>
-          <p className='text-sm mt-5'>
+          <p className='text-sm mt-5 text-gray-500'>
             This is a demo project. You can sign up with your email and password
             or with Google.
           </p>
@@ -93,7 +93,7 @@ export default function SignIn() {
             {/* <OAuth /> */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Don't Have an account?</span>
+            <span className='text-gray-500'>Don't Have an account?</span>
             <Link to='/sign-up' className='text-blue-500'>
               Sign up
             </Link>
