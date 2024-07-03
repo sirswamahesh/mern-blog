95443b79d9ea44b0b218f2dc1601e5fbaccfb8cb
sirswamahesh <sirswamahesh@gmail.com>
2024-07-03 07:51:14 +0530

    fix issue

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ const postRoutes = require('./routes/post.route');
 const commentRoutes = require('./routes/comment.route');
 const connectDB = require("./dbConfig/connectDb");
 const cookieParser = require("cookie-parser");
-const path = require('path');
+
 dotenv.config();
-const __dirname = path.resolve();
+
 const app = express();
 app.use(cookieParser());
 app.use(express.json())
@@ -18,11 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 app.listen(3000, () => {
   try {
